11f234e05bbd7a3c3a00e07bc24139c8acbc221d
Herms Stanley <scatterinc@gmail.com>
2021-09-27 11:14:52 -0400

    timestmap

--- src/views/Timestamp/Timestamphome.vue
+++ src/views/Timestamp/Timestamphome.vue
@@ -1,5 +1,74 @@
 <template>
 <div>
-    test
+    <b-row>
+        <b-col class="border"></b-col>
+    <b-col cols="4" class="border">
+       <b-row  class="mb-2 mt-4" >
+                               <b-col   style="font-size: 1.25rem;" class="bg-light border shadow text-center ml-5 mb-3" md="9" >
+                                  <div class="display">{{current || 'Enter Employee Pin'}}</div>
+                               </b-col>
+                           </b-row>
+                               <b-row
+     v-for="(row, n) in list"
+    :key="`row${n}`"
+    class="text-center center row container-fluid mb-2 mr-4"
+
+  >
+    <b-list-group horizontal="md" class="row px-md-2 flex-fill ml-1" >
+      <b-list-group-item
+        v-for="(item, key) in row"
+        :key="`item${key}`"
+        button
+        class="col mr-1 ml-1 rounded border shadow"
+        value={value}
+                              >
+        <template v-if="item.paynum">
+          <div class="text-center" @click="append(item.number)"><h4>{{ item.paynum }}</h4></div>
+                       </template>
+        <span v-else> {{ item }} </span>
+      </b-list-group-item>
+    </b-list-group>
+      </b-row>
+      <b-row>
+      </b-row>
+
+       <b-row  class="mb-2 mt-1 mr-5 ml-4" >
+           <b-button block size="lg" variant="info">Enter</b-button>
+                                               </b-row>
+
+    </b-col>
+    <b-col cols="2"></b-col>
+    </b-row>
+
 </div>
+
 </template>
+<script>
+export default {
+  name: 'TimeStampHome.vue',
+  data: () => ({
+    list: [
+      [
+        { paynum: '1', type: 'number', number: '1' },
+        { paynum: '2', type: 'number', number: '2' },
+        { paynum: '3', type: 'number', number: '3' }
+      ],
+      [
+        { paynum: '4', type: 'number', number: '4' },
+        { paynum: '5', type: 'number', number: '5' },
+        { paynum: '6', type: 'number', number: '6' }
+      ],
+      [
+        { paynum: '7', type: 'number', number: '7' },
+        { paynum: '8', type: 'number', number: '8' },
+        { paynum: '9', type: 'number', number: '9' }
+      ],
+      [
+        { paynum: '.', type: 'number', number: '.' },
+        { paynum: '0', type: 'number', number: '0' },
+        { paynum: 'X', type: 'text', number: 'clear' }
+      ]
+    ]
+  })
+};
+</script>
