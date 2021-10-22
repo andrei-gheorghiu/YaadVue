88b73174b4cb09eb0ce0b3ec085496f499de407e
Herms Stanley <hstanley@mdcottawa.ca>
2021-10-22 17:20:50 -0400

    modal properties

--- src/components/ReturnProperties.vue
+++ src/components/ReturnProperties.vue
@@ -1,26 +1,53 @@
 <template>
-<b-modal id="ReturnProperties" size="lg" centered title="Return Properties">
-<!---<b-row>
-  <b-col cols="4">
-    Roll Forward from
-  </b-col>
-      <b-col>
-        <b-input/>
-  </b-col>
+<b-modal id="ReturnProperties" size="lg" centered title="Financial Properties">
+<b-row>
+<b-col> <h6>Assignees</h6>
+<div>Preparer</div>
+<div><b-input style="height:20px"/></div>
+<div class="mt-1">Reviewer</div>
+<div><b-input style="height:20px"/></div>
+<div class="mt-1">Partner</div>
+<div><b-input style="height:20px"/></div>
+</b-col>
+<b-col>
+  <h6>Return Status</h6>
+<div>Status</div>
+<div><b-input style="height:20px"/></div>
+<div class="mt-4 text-center">
+     <b-button-group size="sm">
+      <b-button v-b-tooltip.hover title="Review Question" class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'question-circle']"/></b-button>
+      <b-button v-b-tooltip.hover title="notes" class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'sticky-note']"/></b-button>
+      <b-button class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'lock']"/></b-button>
+      <b-button class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'bookmark']"/></b-button>
+      <b-button class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'paperclip']"/></b-button>
+       <b-button class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'hourglass-end']"/></b-button>
+        <b-button class="mr-1" variant="transparent"><font-awesome-icon size="2x" :icon="['fas', 'fire']"/></b-button>
+        </b-button-group>
+
+</div>
+</b-col>
 </b-row>
-<div class="border mt-1 mb-1" />
+              <template #modal-footer>
+        <div>
+               <b-button
+            variant="light"
+            size="sm"
+            class="float-right ml-2"
+           @click="closeModal()"
+          >
+            Cancel
+          </b-button>
+          <b-button
+            variant="info"
+            size="sm"
+            class="float-right"
+            @click="show=false"
+          >
+            Update
+          </b-button>
 
-                    <b-table striped hover :items="items" :fields="fields"  :small="true"
-                    class="Mx-4 mt-2"
-                    >
-    <template v-slot:cell(index)="{ index }">
-      {{ index + 1 }}
+        </div>
       </template>
-                        <template v-slot:cell()="{ item, field: { key }}">
-      <b-form-input v-model="item[key]" />
-    </template>
-
-        </b-table>--->
  </b-modal>
 </template>
 
--- src/components/WsNewEntity.vue
+++ src/components/WsNewEntity.vue
@@ -1,26 +1,65 @@
 <template>
 <b-modal id="WsNewEntity" size="lg" centered title="Add New Entity">
-<!---<b-row>
-  <b-col cols="4">
-    Roll Forward from
-  </b-col>
-      <b-col>
-        <b-input/>
-  </b-col>
+<b-row class="mt-0">
+<b-col>
+  <b-row><b-col class="ml-3" cols="-4">Entity Name</b-col><b-col> <b-input style="height:20px"/></b-col></b-row>
+  <b-row class="mt-3 mb-3"><b-col ><div>Financial year start</div><div><b-input type="date" style="height:20px"/></div></b-col><b-col><div>Financial year end</div><div><b-input type="date" style="height:20px"/></div></b-col></b-row>
+  <b-row>
+<div class="mr-1 ml-1">
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Contact" active>
+        <b-card-text><!---Tab contents 1-->
+
+        <b-row class="mt-1 mb-1"><b-col ><div>Client Name</div><div><b-input  style="height:20px"/></div></b-col><b-col><div>Businees Number</div><div><b-input  style="height:20px"/></div></b-col></b-row>
+        <b-row class="mt-2 mb-1"><b-col ><div>Client code</div><div><b-input  style="height:20px"/></div></b-col><b-col></b-col></b-row>
+        </b-card-text>
+      </b-tab>
+     <!--- <b-tab title="Tab 2">
+        <b-card-text>Tab contents 2</b-card-text>
+      </b-tab>-->
+    </b-tabs>
+  </b-card>
+</div>
+  </b-row>
+</b-col>
+<!---Left side Assignees-->
+<b-col cols="5" class="bg-light border-left">
+<h6>Assignees</h6>
+<div>Preparer</div>
+<div><b-input style="height:20px"/></div>
+<div class="mt-1">Reviewer</div>
+<div><b-input style="height:20px"/></div>
+<div class="mt-1">Partner</div>
+<div><b-input style="height:20px"/></div>
+
+<h6 class="mt-4">Visibility</h6>
+<b-btn :pressed="true" class="mb-2" size="sm" variant="outline-info">Public</b-btn>
+<b-btn :pressed="false" class="mb-2" size="sm" variant="outline-info">Private</b-btn>
+</b-col>
 </b-row>
-<div class="border mt-1 mb-1" />
 
-                    <b-table striped hover :items="items" :fields="fields"  :small="true"
-                    class="Mx-4 mt-2"
-                    >
-    <template v-slot:cell(index)="{ index }">
-      {{ index + 1 }}
+              <template #modal-footer>
+        <div>
+               <b-button
+            variant="light"
+            size="sm"
+            class="float-right ml-2"
+           @click="closeModal()"
+          >
+            Cancel
+          </b-button>
+          <b-button
+            variant="info"
+            size="sm"
+            class="float-right"
+            @click="show=false"
+          >
+            Add Entity
+          </b-button>
+
+        </div>
       </template>
-                        <template v-slot:cell()="{ item, field: { key }}">
-      <b-form-input v-model="item[key]" />
-    </template>
-
-        </b-table>--->
  </b-modal>
 </template>
 
@@ -37,6 +76,7 @@ export default {
       'Enitty Name', 'Enitty Number', 'Financial Year' /* icon */
     ],
     items: []
+
   })
 };
 </script>
